0b0d8cd8e85c68aa1b02f0ed1ee223f9ea5b9a54
javlon <javlonrahimov1212@gmail.com>
2021-12-02 22:56:09 +0500

    day2

--- day2/day2.go
+++ day2/day2.go
@@ -1,13 +1,38 @@
 package main
 
+import (
+	"fmt"
+	"strconv"
+	"strings"
+
+	utils "javlonrahimov/AdventOfCode/utils"
+)
+
 func main() {
-	// your code goes here
+	lines := utils.ReadFileLineByLine("input.txt")
+	fmt.Println(part1(lines))
+
 }
 
-// func part1(lines []string) (depth, length int) {
-// 	for index, line := range lines {
-// 		switch line {
-// 		case ""
-// 		}
-// 	}
-// }
+func part1(lines []string) int {
+	depth, length := 0, 0
+	for _, line := range lines {
+		lineArr := strings.Split(line, " ")
+		switch lineArr[0] {
+		case "forward":
+			result, _ := strconv.Atoi(lineArr[1])
+			length += result
+		case "up":
+			result, _ := strconv.Atoi(lineArr[1])
+			depth -= result
+		case "down":
+			result, _ := strconv.Atoi(lineArr[1])
+			depth += result
+		}
+	}
+	return depth * length
+}
+
+func part2(lines []string) int {
+	return 0
+}
--- day2/day2_test.go
+++ day2/day2_test.go
@@ -1,1 +1,21 @@
 package main
+
+import (
+	"testing"
+	utils "javlonrahimov/AdventOfCode/utils"
+)
+
+func TestDay2(t *testing.T) {
+
+	lines := utils.ReadFileLineByLine("input_test.txt")
+
+	t.Run("part 1", func(t *testing.T) {
+		got := part1(lines)
+		want := 150
+
+		if got != want {
+			t.Errorf("got %d want %d", got, want)
+		}
+	})
+
+}
