8aaae6a01b6fb642156810602887497ae44d837b
javlon <javlonrahimov1212@gmail.com>
2021-12-07 09:49:53 +0500

    add day 6

---
+++ day6/day6.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"fmt"
+	"javlonrahimov/AdventOfCode/utils"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	lines := utils.ReadFile("input.txt")
+	fmt.Println(part1(lines, 80))
+	fmt.Println(part2(lines))
+}
+
+func part1(input string, days int) int {
+	list := parseStringToList(input)
+
+	for i := 0; i < days; i++ {
+		addCount := 0
+		for j := 0; j < len(list); j++ {
+			if list[j] > 0 {
+				list[j]--
+			} else {
+				list[j] = 6
+				addCount++
+			}
+		}
+		for j := 0; j < addCount; j++ {
+			list = append(list, 8)
+		}
+	}
+	return len(list)
+}
+
+func part2(input string) int64 {
+
+	school := parseStringToList(input)
+
+	counts := map[int64]int{}
+	for _, timer := range school {
+		counts[timer] += 1
+	}
+
+	for day := 1; day <= 256; day++ {
+		// school = util.FlatMap(school, AdvanceOne)
+		nextCounts := map[int64]int{}
+		for timer, count := range counts {
+			if timer == 0 {
+				nextCounts[6] += count
+				nextCounts[8] += count
+			} else {
+				nextCounts[timer-1] += count
+			}
+		}
+		counts = nextCounts
+
+		schoolSize := 0
+		for _, count := range counts {
+			schoolSize += count
+		}
+
+		if day == 256 {
+			return int64(schoolSize)
+		}
+	}
+	return 0
+}
+
+
+func parseStringToList(input string) []int64 {
+	stringList := strings.Split(input, ",")
+	intList := make([]int64, len(stringList))
+	for i, s := range stringList {
+		intList[i] = toInt(s)
+	}
+
+	return intList
+}
+
+func toInt(s string) int64 {
+	n, err := strconv.Atoi(s)
+	if err != nil {
+		panic(err)
+	}
+	return int64(n)
+}
+
---
+++ day6/day6_test.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"javlonrahimov/AdventOfCode/utils"
+	"testing"
+)
+
+func TestDay6(t *testing.T) {
+	lines := utils.ReadFile("input_test.txt")
+
+	t.Run("part 1", func(t *testing.T) {
+		got := part1(lines, 18)
+		want := 26
+
+		if got != want {
+			t.Errorf("got %d want %d", got, want)
+
+		}
+	})
+
+	t.Run("part 2", func(t *testing.T) {
+		got := part2(lines)
+		want := int64(26984457539)
+
+		if got != want {
+			t.Errorf("got %d want %d", got, want)
+
+		}
+	})
+}
