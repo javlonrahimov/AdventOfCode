71393f4a0d4b1c718ae8bebc5ffc35cbdc0fbb5d
javlon <javlonrahimov1212@gmail.com>
2021-12-23 01:32:36 +0500

    test

---
+++ day9.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Day 9:")
+}
+
+func part1(lines []string) int {
+	return 0
+}
--- day9/day9_test.go
+++ day9/day9_test.go
@@ -1,7 +1,17 @@
 package main
 
-import "testing"
+import (
+	"javlonrahimov/AdventOfCode/utils"
+	"testing"
+)
 
-func TestDay9(t *testing.T){
+func TestDay9(t *testing.T) {
+	lines := utils.ReadFileLineByLine("input_test.txt")
 
+	got := part1(lines)
+	want := 15
+
+	if got != want {
+		t.Errorf("want '%d' got '%d", want, got)
+	}
 }
